de5ca50ee89377e0ea0db1e44a747e5b92604efc
sergey s <som-ana@yandex.ru>
2023-04-23 22:50:19 +0300

    adding a test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,7 +10,21 @@ public class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12,};
+
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findM() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {0, 12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12,};
+
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
